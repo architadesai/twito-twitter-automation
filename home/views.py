f912c947287f2d2e8f3b8741bf8fee48073c840a
Yashashvi Dave <yashashvidave@gmail.com>
2017-07-23 20:43:58 +0530

    ApplicationLogin Page Only Authenticated User

--- home/views.py
+++ home/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import (
     render,
 
 )
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 
 def index(request):
     return render(request, 'home/index.html')
 
 
+@login_required
+def applicationlogin(request):
+
+    return HttpResponse("You are authenticated user ...")
--- twito2/urls.py
+++ twito2/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from home.views import index
+from home.views import (
+    index,
+    applicationlogin,
+)
 
 urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', index, name="HomePage")
+    url(r'^$', index, name="HomePage"),
+
+    url(r'^login/', applicationlogin, name="ApplicationLogin"),
 
 ]
