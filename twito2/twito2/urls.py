2b4e2059b6b714415d3d081794c8d87bb5cd60ba
Yashashvi Dave <yashashvidave@gmail.com>
2017-07-24 18:04:12 +0530

    First Page with Design

--- twito2/twito2/urls.py
+++ twito2/twito2/urls.py
@@ -13,9 +13,9 @@ urlpatterns = [
 
     url(r'^$', index, name="HomePage"),
 
-    url(r'^login/$', applicationlogin, name="ApplicationLogin"),
+    url(r'^dashboard/$', applicationlogin, name="ApplicationLogin"),
 
-    url(r'^user/(?P<appname>\w+)/$', applicationaccess, name="ApplicationAccess"),
+    url(r'^dashboard/(?P<appname>\w+)/$', applicationaccess, name="ApplicationAccess"),
 
 
 ]
@@ -23,3 +23,5 @@ urlpatterns = [
 
 ######################UPDATE login link to specific user name ###############
 ######################UPDATE app link to specific app name###############
+############Also to make sure that login user should not be able to check other user acc by line####################
+################Add login page to beginning, instead of admin login#####################
