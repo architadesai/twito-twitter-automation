14267c36dc43b8c773f7f457d335d27f17a40dec
Archita Desai <architad8@gmail.com>
2017-08-15 20:00:09 +0530

    Remove twito2 (old)

--- twito2/home/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import ApplicationData, ApplicationAccess, UserActivity
-
-admin.site.register(ApplicationData)
-admin.site.register(ApplicationAccess)
-admin.site.register(UserActivity)
